10cff5b5c00e7f2dcbdb26124f93c0a4c6037e24
hongphucnthp00@gmail.com <hongphucnthp00@gmail.com>
2023-05-10 18:33:12 +0700

    Update User

---
+++ src/pages/User/UserPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header/header'
+import Footer from '../../components/Footer/footer'
+import User from '../../components/Header/User/user'
+function UserPage() {
+  return (
+    <div>
+        <Header/>
+        <User/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default UserPage
