70589e75d553c94cfea86e451d9611bcafad9e77
NguyenThanhLich <Thanhlichthinguyen@gmail.com>
2023-05-23 14:14:35 +0700

    hihi

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -4,12 +4,14 @@ import ListCV from '../../components/ListCV/listCV'
 import Footer from '../../components/Footer/footer'
 import ModalView from '../../components/ListCV/Modal/modalView'
 import BoxText from "../../components/BoxText/boxText"
+import Ins from "../../components/InstructContainer/insContainer"
 function Home() {
   return (
     <div>
         <Header/>
         <ListCV/>
         <BoxText/>
+        <Ins/>
         <Footer/>
     </div>
   )
