91683bec0b4c572f400776a4677754c49ae07479
Van-anh0 <lnvananh2308@gmail.com>
2023-05-28 10:34:39 +0700

    update

--- src/components/HistoryCV/HistoryCV.js
+++ src/components/HistoryCV/HistoryCV.js
@@ -8,6 +8,7 @@ import { handleDeleteCVById } from '../../hook/data/deleteData';
 import { AuthContext } from '../../App';
 import { momentDayMonthYear } from '../../hook/moment/formatDay';
 import { ToastContainer, toast } from 'react-toastify';
+import jsPDF from 'jspdf';
 
 function HistoryCV() {
   const { infoUser } = useContext(AuthContext);
@@ -32,6 +33,22 @@ function HistoryCV() {
     // load lại component khi mà reload và listData thay đổi
   }, [reload, listData]);
 
+  const handlePrint = (statusCV) => {
+   
+      //Tạo đối tượng PDF
+      const pdf = new jsPDF({
+        orientation: 'portrait',
+        unit: 'mm',
+        format: 'a4',
+      });
+
+      // Thêm ảnh vào PDF
+      pdf.addImage(statusCV, 'PNG', 0, 0, 210, 297); // 190mm = A4 width - 20mm margin
+      const fileName = 'CV-online.pdf';
+      // Lưu PDF trong callback của html2canvas
+      pdf.save(fileName);
+    
+  };
   return (
     <div className='history_CV'>
       <ToastContainer />
@@ -40,10 +57,10 @@ function HistoryCV() {
       <div className='list_history_components'>
         {listData && Array.isArray(listData) && listData.length > 0 ? (
           listData?.map((cv) => {
-            const src = `data:image/jpeg;base64,${cv.avatarCV}`;
+            // const src = `data:image/jpeg;base64,${cv.avatarCV}`;
             return (
               <div key={`cv-${cv._id}`} className='history_component'>
-                <img src={src} alt='' />
+                <img src={cv.statusCV} alt='mẫu CV' />
                 <div className='custom_history'>
                   <div className='content'>
                     <div className='title'>
@@ -54,7 +71,7 @@ function HistoryCV() {
                       <div className='btn'>
                         <ImPencil2 /> Sửa CV
                       </div>
-                      <div className='btn bg'>
+                      <div className='btn bg' onClick={() => handlePrint(cv.statusCV)}>
                         <FiDownload /> Tải xuống
                       </div>
                     </div>
